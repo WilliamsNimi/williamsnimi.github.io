7528c61585a169b98a687bfd026aec6f0b4934ce
williamson.nimi@gmail.com <williamson.nimi@gmail.com>
2017-06-03 18:41:16 +0100

    Contact form deleted

--- php/db.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$connection = mysqli_connect("localhost", "root", "");// Establishing Connection with Server by passing server_name, user_id and password as a parameter
-	$db = mysqli_select_db($connection, "rentaplace"); //select database
-?>
